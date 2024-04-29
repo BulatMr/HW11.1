6d97158c363cabaa5463c37d2cd3da33be6b64b2
morz <morzag1@icloud.com>
2024-04-29 16:49:34 +0400

    import func summ

---
+++ main.py
@@ -0,0 +1,11 @@
+from funcs import test_funcs_summ
+
+
+def main():
+    a = int(input("Введите первое число: \n"))
+    b = int(input("Введите второе число: \n"))
+    print(test_funcs_summ(a, b))
+
+
+if __name__ == "__main__":
+    main()
