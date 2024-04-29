6f1c911af1aeb5428145505da1f28aa784a31521
morz <morzag1@icloud.com>
2024-04-29 16:50:52 +0400

    add func sub

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from funcs import test_funcs_summ
+from funcs import test_funcs_summ, test_funcs_sub
 
 
 def main():
     a = int(input("Введите первое число: \n"))
     b = int(input("Введите второе число: \n"))
     print(test_funcs_summ(a, b))
+    print(test_funcs_sub(a, b))
 
 
 if __name__ == "__main__":
